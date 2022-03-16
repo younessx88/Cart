b962d7389f9f79a1227d7202eee7a55c6c070cf5
Zouhair CHAKIR <zchakir11@gmail.com>
2022-03-16 21:56:52 +0100

    Implementing Promotions

--- src/main/java/com/nespresso/sofa/interview/cart/services/PromotionEngine.java
+++ src/main/java/com/nespresso/sofa/interview/cart/services/PromotionEngine.java
@@ -1,5 +1,9 @@
 package com.nespresso.sofa.interview.cart.services;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.nespresso.sofa.interview.cart.model.Cart;
 
 /**
@@ -7,8 +11,38 @@ import com.nespresso.sofa.interview.cart.model.Cart;
  * For each 10 products purchased a gift with code 7000 is offer.
  */
 public class PromotionEngine {
-
     public Cart apply(Cart cart) {
-        return cart;
+        Map<String, Integer> temp = new HashMap<>(cart.getProducts());
+        temp = addPromotion(temp);
+        temp = addGift(temp);
+        return new Cart(temp);
+    }
+
+    private Map<String,Integer> addPromotion(Map<String,Integer> products) {
+        if (!products.containsKey("1000")) {
+            products.remove("9000");
+        } else {
+            products.put("9000", 1);
+        }
+        return products;
+    }
+
+    private Integer maxValue(Map<String,Integer> products) {
+        Map.Entry<String, Integer> maxEntry = Collections.max(products.entrySet(),
+            (Map.Entry<String, Integer> e1, Map.Entry<String, Integer> e2) -> e1.getValue()
+                .compareTo(e2.getValue()));
+        return maxEntry.getValue();
+    }
+
+    private Map<String,Integer> addGift(Map<String,Integer> products) {
+        if (!products.isEmpty()) {
+            Integer max = maxValue(products);
+            if (max >= 10) {
+                products.put("7000", max / 10);
+            } else {
+                products.remove("7000");
+            }
+        }
+        return products;
     }
 }
