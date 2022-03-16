9f4af84ead5c763d68b7c55ab9c3795c67916ec4
hachcharif <hachcharif@sqli.com>
2022-03-16 15:35:30 +0100

    FixCartTest

--- src/main/java/com/nespresso/sofa/interview/cart/model/Cart.java
+++ src/main/java/com/nespresso/sofa/interview/cart/model/Cart.java
@@ -3,23 +3,25 @@ package com.nespresso.sofa.interview.cart.model;
 import static java.util.UUID.randomUUID;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public final class Cart implements Serializable {
 
     private final UUID id;
-
+    private  final Map<String, Integer> products;
     public Cart() {
         this(randomUUID());
     }
 
     public Cart(UUID id) {
-        this.id = id;
+        this(new HashMap<>());
     }
 
     public Cart(Map<String, Integer> products) {
         this.id = randomUUID();
+        this.products=products;
     }
 
     public UUID getId() {
@@ -27,7 +29,14 @@ public final class Cart implements Serializable {
     }
 
     public Map<String, Integer> getProducts() {
-        return null;
+        return this.products;
     }
 
+    @Override
+    public String toString() {
+        return "Cart {" +
+            "id: " + id +
+            ", products: " + products +
+            '}';
+    }
 }
