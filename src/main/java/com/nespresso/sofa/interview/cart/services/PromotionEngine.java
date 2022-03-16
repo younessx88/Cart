fc7c1f9e04b8087bf2ace246d243175e0a51336c
OmaimaEl-youssefy <omaimaelyo@gmail.com>
2022-03-17 00:10:07 +0100

    fixed PromotionEngine

--- src/main/java/com/nespresso/sofa/interview/cart/services/PromotionEngine.java
+++ src/main/java/com/nespresso/sofa/interview/cart/services/PromotionEngine.java
@@ -1,5 +1,8 @@
 package com.nespresso.sofa.interview.cart.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.nespresso.sofa.interview.cart.model.Cart;
 
 /**
@@ -8,7 +11,42 @@ import com.nespresso.sofa.interview.cart.model.Cart;
  */
 public class PromotionEngine {
 
+    public static final String PRODUCT_WITH_PROMOTION = "1000";
+    public static final String PROMOTION = "9000";
+    public static final String GIFT = "7000";
+
     public Cart apply(Cart cart) {
+
+        Map<String, Integer> products = new HashMap<>(cart.getProducts());
+
+        int nbOfProducts = getQuantity(cart);
+
+        if (products.containsKey(PRODUCT_WITH_PROMOTION)) {
+            products.put(PROMOTION, 1);
+        } else if (products.containsKey(PROMOTION)) {
+            products.remove(PROMOTION);
+        }
+
+        if (nbOfProducts >= 10) {
+            int quantity = nbOfProducts / 10;
+            products.put(GIFT, quantity);
+        }
+
+        if (nbOfProducts < 10 && products.containsKey(GIFT)) {
+            products.remove(GIFT);
+        }
+
+        cart.setAllProducts(products);
+
         return cart;
     }
+
+    private int getQuantity(Cart cart) {
+        int result = 0;
+        for (int value : cart.getProducts().values()) {
+            result += value;
+        }
+        return result;
+    }
 }
+
