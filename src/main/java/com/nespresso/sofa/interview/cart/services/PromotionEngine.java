bdee7cd730d821bc66087a55cae35670ee68dcfa
Mohamadelattar <mohamadelattar99@gmail.com>
2022-03-16 21:55:26 +0100

    "Edit Cart - CartService - PromotionEngine "

--- src/main/java/com/nespresso/sofa/interview/cart/services/PromotionEngine.java
+++ src/main/java/com/nespresso/sofa/interview/cart/services/PromotionEngine.java
@@ -1,5 +1,7 @@
 package com.nespresso.sofa.interview.cart.services;
 
+import java.util.Map;
+
 import com.nespresso.sofa.interview.cart.model.Cart;
 
 /**
@@ -7,8 +9,13 @@ import com.nespresso.sofa.interview.cart.model.Cart;
  * For each 10 products purchased a gift with code 7000 is offer.
  */
 public class PromotionEngine {
-
+    public static final String PRODUCT_WITH_PROMOTION = "1000";
+    public static final String PROMOTION = "9000";
+    public static final String GIFT = "7000";
     public Cart apply(Cart cart) {
+        if(cart.getProducts().containsKey(PRODUCT_WITH_PROMOTION)) {
+            cart.setProducts(PROMOTION,1);
+        }
         return cart;
     }
 }
