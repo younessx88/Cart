3511e8e4c81d9d51b8b02751d75c6528eaf7a651
Zouhair CHAKIR <zchakir11@gmail.com>
2022-03-16 16:33:05 +0100

    Implementing CartService

--- src/main/java/com/nespresso/sofa/interview/cart/services/CartService.java
+++ src/main/java/com/nespresso/sofa/interview/cart/services/CartService.java
@@ -2,8 +2,9 @@ package com.nespresso.sofa.interview.cart.services;
 
 import java.util.UUID;
 
-import com.nespresso.sofa.interview.cart.model.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nespresso.sofa.interview.cart.model.Cart;
 
 public class CartService {
 
@@ -25,15 +26,17 @@ public class CartService {
      */
     public boolean add(UUID cartId, String productCode, int quantity) {
         final Cart cart = cartStorage.loadCart(cartId);
-        if(quantity>0) {
-            cart.getProducts().put(productCode, quantity);
+
+        if (quantity > 0) {
+            cart.getProducts().put(productCode, cart.getProducts().getOrDefault(productCode, 0) + quantity);
             cartStorage.saveCart(cart);
             return true;
-        }else{
+        } else {
             return false;
         }
-
     }
+
+
 
     /**
      * Set a quantity of a product to the cart and store the cart
@@ -48,17 +51,34 @@ public class CartService {
      */
     public boolean set(UUID cartId, String productCode, int quantity) {
         final Cart cart = cartStorage.loadCart(cartId);
-        cartStorage.saveCart(cart);
+        if (cart.getProducts().getOrDefault(productCode, 0) != quantity) {
+            if (quantity > 0) {
+                cart.getProducts().put(productCode, quantity);
+                cartStorage.saveCart(cart);
+                return true;
+            } else if (quantity == 0) {
+                cart.getProducts().remove(productCode);
+                return true;
+            } else {
+                if(Math.abs(quantity)<cart.getProducts().getOrDefault(productCode, 0)){
+                    cart.getProducts().put(productCode,cart.getProducts().getOrDefault(productCode, 0)-quantity);
+                    return true;
+                }else if(Math.abs(quantity)>=cart.getProducts().getOrDefault(productCode, 0)){
+                    cart.getProducts().remove(productCode);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
-    /**
-     * Return the card with the corresponding ID
-     *
-     * @param cartId
-     * @return
-     */
-    public Cart get(UUID cartId) {
-        return cartStorage.loadCart(cartId);
+        /**
+         * Return the card with the corresponding ID
+         *
+         * @param cartId
+         * @return
+         */
+        public Cart get (UUID cartId){
+            return cartStorage.loadCart(cartId);
     }
 }
